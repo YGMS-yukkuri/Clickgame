4ea636454702f3b5d1988211821c8ae6cf0ceddd
YGMSch <reityan.sakura0104@gmail.com>
2025-06-06 21:18:36 +0900

    初期化変数の追加

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ const AutoUpgrade1 = document.querySelector('#AutoUpgrade1');
 const AutoUpgrade2 = document.querySelector('#AutoUpgrade2');
 const AutoUpgrade3 = document.querySelector('#AutoUpgrade3');
 const AutoUpgrade4 = document.querySelector('#AutoUpgrade4');
-let zouka;
-let count;
+let zouka = 0;
+let count = 0;
+let auto1 = 0;
+let auto2 = 0;
+let auto3 = 0;
+let auto4 = 0;
+let clickUpgCost = 10;
+let auto1Cost = 10;
+let auto2Cost = 10;
+let auto3Cost = 10;
+let auto4Cost = 10;
 mainbutton.addEventListener('click', function(){
     console.log("mainButton");
-    
+    mainClick();
 });
 ClickUpgrade.addEventListener('click', function(){
     console.log("UpgradeButton")
@@ -25,3 +34,7 @@ AutoUpgrade3.addEventListener('click', function(){
 AutoUpgrade4.addEventListener('click', function(){
     console.log("UPG4")
 })
+
+function mainClick(){
+    count = zouka + 1;
+}
