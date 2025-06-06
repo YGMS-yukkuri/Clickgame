676af28960ee00d6166e361ac3c53ad44b9ebf2f
yukkuri <reityan.sakura0104@gmail.com>
2025-06-06 18:01:48 +0900

    アップグレードUIの作成

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const mainbutton = document.querySelector("#mainbtn");
-let zouka
-let count
+const ClickUpgrade = document.querySelector("#ClickUpgrade");
+const AutoUpgrade1 = document.querySelector('#')
+let zouka;
+let count;
 mainbutton.addEventListener('click', function(){
-    console.log("AAAAA");
+    console.log("mainButton");
     
+});
+ClickUpgrade.addEventListener('click', function(){
+    console.log("UpgradeButton")
 });
