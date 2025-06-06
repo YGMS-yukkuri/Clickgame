a660205e62efc233851d2e231607b395f5dc298c
yukkuri <reityan.sakura0104@gmail.com>
2025-06-06 18:02:34 +0900

    queryselectorの設定

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const mainbutton = document.querySelector("#mainbtn");
 const ClickUpgrade = document.querySelector("#ClickUpgrade");
-const AutoUpgrade1 = document.querySelector('#')
+const AutoUpgrade1 = document.querySelector('#AutoUpgrade1');
+const AutoUpgrade2 = document.querySelector('#AutoUpgrade2');
+const AutoUpgrade3 = document.querySelector('#AutoUpgrade3');
+const AutoUpgrade4 = document.querySelector('#AutoUpgrade4');
 let zouka;
 let count;
 mainbutton.addEventListener('click', function(){
@@ -10,3 +13,15 @@ mainbutton.addEventListener('click', function(){
 ClickUpgrade.addEventListener('click', function(){
     console.log("UpgradeButton")
 });
+AutoUpgrade1.addEventListener('click', function(){
+    console.log("UPG1")
+});
+AutoUpgrade2.addEventListener('click', function(){
+    console.log("UPG2")
+});
+AutoUpgrade3.addEventListener('click', function(){
+    console.log("UPG3")
+});
+AutoUpgrade4.addEventListener('click', function(){
+    console.log("UPG4")
+})
